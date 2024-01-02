c39a9d653474ca383ea84f9f5d6c67458290a1c7
FeryazHacimuradov <tu201906125@code.edu.az>
2024-01-02 11:16:40 +0400

    Add project files.

---
+++ StudentApp/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.Models;
+using System.Numerics;
+
+namespace StudentApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>(){ new Student
+            {
+                Id = 1,
+                StudentName = "Student1",
+                Email = "student1@gmail.com",
+                Address = "student1adress"
+            },
+            new Student
+            {
+                Id = 2,
+                StudentName = "Student2",
+                Email = "student2@gmail.com",
+                Address = "student2adress"
+            },
+            };
+        }
+    }
+}
